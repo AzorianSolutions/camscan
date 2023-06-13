0b74e6d56277e001dbd45ef232eab94acd9d9761
Matt Scott <matt@azorian.solutions>
2023-06-13 08:43:45 -0400

    Updated Go build script to use new package paths.

--- bin/build.sh
+++ bin/build.sh
@@ -3,8 +3,8 @@
 # Argument #1: Root path of project, current directory assumed when not defined
 root_path=$(readlink -f "${1:-$PWD}")
 
-# Argument #2: Go program path to build, "as/camscan/camscan.go" assumed when not defined
-build_path="${2:-as/camscan/camscan.go}"
+# Argument #2: Go program path to build, "internal/camscan/camscan.go" assumed when not defined
+build_path="${2:-internal/camscan/camscan.go}"
 
 # Argument #3: The platforms and architectures to build binaries for
 builds="${3:-aix=ppc64|android=386,amd64,arm,arm64|darwin=amd64,arm64|dragonfly=amd64|freebsd=386,amd64,arm,arm64,riscv64|illumos=amd64|ios=amd64,arm64|js=wasm|linux=386,amd64,arm,arm64,loong64,mips,mips64,mips64le,mipsle,ppc64,ppc64le,riscv64,s390x|netbsd=386,amd64,arm,arm64|openbsd=386,amd64,arm,arm64,mips64|plan9=386,amd64,arm|solaris=amd64|windows=386,amd64,arm,arm64}"
@@ -29,9 +29,12 @@ do
      :
      printf "  ∟₋₋ Building Architecture: %s\n" "$architecture"
      output_path="$bin_path/camscan-$platform-$architecture"
+     if [ "$platform" == "windows" ]; then
+       output_path="$output_path.exe"
+     fi
      export GOOS="$platform"
      export GOARCH="$architecture"
-     go build -trimpath -C "$root_path" -o "$output_path" "$build_path"
+     go build -trimpath -o "$output_path" "$build_path"
      printf "  ∟₋₋ Built %s to %s\n" "$architecture" "$output_path"
    done
    printf "\n"
